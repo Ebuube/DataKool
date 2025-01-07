5cb1d533c956faa5db02164d3eb4f553fd4196e0
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2025-01-07 23:48:08 +0100

    Created ResourceForm component static

---
+++ frontend/src/Forms/ResourceForm.js
@@ -0,0 +1,43 @@
+import { Component } from "react";
+import { Container, Grid } from "semantic-ui-react";
+
+class ResourceForm extends Component {
+  /* Sample format
+  const form_format = {
+      fields: {
+        fullname: true,
+        gender: true,
+        telephone: true,
+        guardian_phone: true,
+        email: true,
+        address: true,
+        jamb_reg_no: true,
+        university: true,
+        toggle_check_box: true,
+        last_updated: true,
+      },
+      buttons: {
+        orientation: 'vertical',
+        names: [
+          'Print',
+          'Save',
+        ]
+      },
+    };
+  */
+  render() {
+    const { buttons, fields } = this.props.format;
+    return (
+      <Container as={Grid}>
+        <Grid.Column width={13}>
+          FormFields
+        </Grid.Column>
+        <Grid.Column width={3}>
+          ActionButtons
+        </Grid.Column>
+      </Container>
+    );
+  };
+};
+
+export default ResourceForm;
