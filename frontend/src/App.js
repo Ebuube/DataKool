165da71ce5642ad46a8b8f6655ca2157df497dfc
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2025-01-08 21:47:55 +0100

    Created StudentCreate component static

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Login from './Login/Login.js';
 import Layout from './Layout/Layout.js';
+import StudentCreate from './Student/StudentCreate.js';
 import Profile from './Profile/Profile.js';
 import Admin from './Admin/Admin.js';
 import {
@@ -17,6 +18,7 @@ class App extends Component {
               {/* <Login /> */}
               {/* <Profile /> */}
               <Route path='/login' element={<Login />} />
+              <Route path='/students/new' element={<StudentCreate />} />
               <Route path='/profile' element={<Profile />} />
               <Route path='/admin' element={<Admin />} />
               <Route path='*' element={<h1>No match component...</h1>} />
---
+++ frontend/src/Student/StudentCreate.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import ResourceForm from "../Forms/ResourceForm";
+
+class StudentCreate extends Component {
+  render() {
+    const form_format = {
+      fields: {
+        fullname: true,
+        gender: true,
+        telephone: true,
+        guardian_phone: true,
+        email: true,
+        address: true,
+        jamb_reg_no: true,
+        university: true,
+      },
+      buttons: {
+        orientation: 'vertical',
+        names: [
+          'Save',
+        ],
+      },
+    };
+    return(
+      <ResourceForm format={form_format} />
+    );
+  };
+};
+
+export default StudentCreate;
