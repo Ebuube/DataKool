fddf74f2c1c77c4b21dcfe95dbfff2a8e89e32e4
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2025-01-10 06:23:15 +0100

    Created FileSearch component static

--- frontend/src/File/FileSearch.js
+++ frontend/src/File/FileSearch.js
@@ -1,18 +1,36 @@
 import { Component } from "react";
 import ResourceSearch from "../Resources/ResourceSearch";
+import ResourceTable from "../Resources/ResourceTable";
 
 class FileSearch extends Component {
   render() {
     const form_format = {
       type: 'file'
     };
+    const table_format = {
+      headers: [
+        'Id',
+        'File Name',
+        'Type',
+        'Owner',
+      ],
+      type: 'files',
+      rows: [
+        [ 1, 'Resume', '.pdf', 'Emeka Odera', ],
+        [ 2, 'Memorandum', '.docx', 'Chidi Ifedi', ],
+        [ 3, 'Sitaled-craft', '.png', 'Ebube Onwuta', ],
+        [ 4, 'Browse Symptoms and Signs ...', '.pdf', 'Gozie Okafor', ],
+      ],
+    }
 
     return(
       <div>
         <ResourceSearch
           format={form_format}
         />
-        <p>ResourceTable</p>
+        <ResourceTable
+          format={table_format}
+        />
       </div>
     );
   };
