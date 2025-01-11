2d0611a61859326451c5b00932d2cdacb12cd6da
Ebube Gideon Onwuta <onwutaebubegideon1555@gmail.com>
2025-01-11 07:46:01 +0100

    Added Footer component to Login component

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -1,32 +1,37 @@
 import React, { Component } from "react";
-import { Grid, GridColumn, GridRow, Image } from "semantic-ui-react";
+import { Divider, Grid, GridColumn, GridRow, Image } from "semantic-ui-react";
 import LoginForm from './LoginForm.js';
 import './Login.css';
+import Footer from "../Layout/Footer.js";
 
 class Login extends Component {
   render() {
     return (
-      <Grid>
-        <GridColumn width={7}>
-          <Image
-            className="img-to-middle"
-            fluid
-            src='/Login/side-image.svg'></Image>
-        </GridColumn>
-        <GridColumn width={9}>
-          <Grid>
-            <GridRow centered>
-              <Image
-                className='img-to-middle'
-                src='/logo.png'
-              />
-            </GridRow>
-            <GridRow>
-              <LoginForm />
-            </GridRow>
-          </Grid>
-        </GridColumn>
-      </Grid>
+      <div>
+        <Grid>
+          <GridColumn width={7}>
+            <Image
+              className="img-to-middle"
+              fluid
+              src='/Login/side-image.svg'></Image>
+          </GridColumn>
+          <GridColumn width={9}>
+            <Grid>
+              <GridRow centered>
+                <Image
+                  className='img-to-middle'
+                  src='/logo.png'
+                />
+              </GridRow>
+              <GridRow>
+                <LoginForm />
+              </GridRow>
+            </Grid>
+          </GridColumn>
+        </Grid>
+        <Divider />
+        <Footer />
+      </div>
     );
   };
 };
